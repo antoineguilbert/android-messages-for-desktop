4a6d211f66e30f3648481ba5e2bd2de84448b57a
Johannes Kann <19167839+Ztarbox@users.noreply.github.com>
2018-07-13 16:17:54 +0200

    support windows local pattern

--- translations/i18n.js
+++ translations/i18n.js
@@ -9,8 +9,10 @@ let app = electron.app;
 module.exports = i18n;
 
 function i18n() {
-  if(fs.existsSync(path.join(__dirname, app.getLocale() + '.js'))) {
-    loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, app.getLocale() + '.js'), 'utf8'));
+  // split locals like de-DE or en-US down to the first language code
+  let local = app.getLocale().split("-")[0];
+  if(fs.existsSync(path.join(__dirname, local + '.js'))) {
+    loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, local + '.js'), 'utf8'));
   }else{
     loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, 'en.js'), 'utf8'));
   }
