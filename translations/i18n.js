4c22db86119429522adec978b301274598e85508
Ztarbox <19167839+Ztarbox@users.noreply.github.com>
2018-07-17 20:48:44 +0200

    fix typo

--- translations/i18n.js
+++ translations/i18n.js
@@ -10,9 +10,9 @@ module.exports = i18n;
 
 function i18n() {
   // split locals like de-DE or en-US down to the first language code
-  let local = app.getLocale().split("-")[0];
+  let locale = app.getLocale().split("-")[0];
   if(fs.existsSync(path.join(__dirname, local + '.js'))) {
-    loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, local + '.js'), 'utf8'));
+    loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, locale + '.js'), 'utf8'));
   }else{
     loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, 'en.js'), 'utf8'));
   }
