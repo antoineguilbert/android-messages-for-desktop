4ca0e2dc7555f61f9d374899d9500918ffdab917
Ztarbox <19167839+Ztarbox@users.noreply.github.com>
2018-07-17 20:49:01 +0200

    Update i18n.js

--- translations/i18n.js
+++ translations/i18n.js
@@ -11,7 +11,7 @@ module.exports = i18n;
 function i18n() {
   // split locals like de-DE or en-US down to the first language code
   let locale = app.getLocale().split("-")[0];
-  if(fs.existsSync(path.join(__dirname, local + '.js'))) {
+  if(fs.existsSync(path.join(__dirname, locale + '.js'))) {
     loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, locale + '.js'), 'utf8'));
   }else{
     loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, 'en.js'), 'utf8'));
